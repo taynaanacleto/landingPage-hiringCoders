8c232367a3b66dc1bc94cbccd4ea568650e100e4
Tayna <tayna_ag@outlook.com>
2021-07-18 19:40:22 -0300

    alteração validação de email

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 function save_email() {
-    var input = document.getElementById("email");
+  var input = document.getElementById("email");
+  if (validaEmail(input.value)) {
     localStorage.setItem("email", input.value);
-   
     Swal.fire({
-        title: 'Sucesso',
-        text: 'Acesse sua caixa de mensagem e confira as ofertas mágicas que enviamos para você!',
-        type: 'success',
-        confirmButtonText: 'ok'
-      })
+      title: 'Sucesso',
+      text: 'Acesse sua caixa de mensagem e confira as ofertas mágicas que enviamos para você!',
+      type: 'success',
+      confirmButtonText: 'ok'
+    })
+  }else{
+    Swal.fire({
+      title: 'Atenção',
+      text: 'Por favor digite um e-mail válido!',
+      type: 'warning',
+      confirmButtonText: 'ok'
+    })
+  }
+}
+function validaEmail(email) {
+  const emailRegex = /^([a-zA-Z][^<>\"!@[\]#$%¨&*()~^:;ç,\-´`=+{}º\|/\\?]{1,})@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  console.log(emailRegex.test(String(email).toLowerCase()))
+  return emailRegex.test(String(email).toLowerCase())
 }
 
